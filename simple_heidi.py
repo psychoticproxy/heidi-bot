1a267fbb5e192be2b817cfaac649ff875705e525
psychoticproxy <ironside_taken@protonmail.com>
2025-10-14 08:26:37 +0000

    Update simple_heidi.py

--- simple_heidi.py
+++ simple_heidi.py
@@ -3,7 +3,6 @@ import os
 import asyncio
 import random
 import logging
-import httpx
 from dotenv import load_dotenv
 from simplified_memory import ConversationMemory
 from engagement import EngagementEngine
